56466bb0daab03653739a2deba92198386747c52
mmcgarry13 <mcgarry31@outlook.com>
2024-11-12 11:51:43 -0500

    Added text input field to search github users and random user button

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,17 +1,7 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
-// import VITE_GITHUB_TOKEN from '../../environment/.env';
+import VITE_GITHUB_TOKEN from '../../environment/.env';
 const GITHUB_API_URL = "https://api.github.com";
-
-
-interface GithubRepo {
-  id: number;
-  name: string;
-  full_name: string;
-  html_url: string;
-  description: string;
-  [key: string]: any;
-}
 
 const CandidateSearch = () => {
 
@@ -21,6 +11,25 @@ const CandidateSearch = () => {
     setInputValue(event.target.value); // event handler for the html input field
   };
 
+  const handleEnterPress = () => {
+    searchGithubUser(inputValue);
+    console.log("Input:", inputValue);
+    // Here you can add any other functionality you want to perform with the input value
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleEnterPress();
+    }
+  };
+
+  const handleButtonClick = () => {
+    searchGithub();
+    console.log("Button clicked! Searching random candidate");
+    // Add the separate functionality you want for this button
+  };
+
+
   return <div>
     <h1>CandidateSearch</h1>;
     <input
@@ -28,10 +37,12 @@ const CandidateSearch = () => {
       id="textInput"
       name="value"
       onChange={handleChange}
+      onKeyDown={handleKeyDown}
       value={inputValue}
       placeholder="Search for a candidate"
     />
     <p>Current Input: {inputValue}</p>
+    <button onClick={handleButtonClick}>Search Random Candidate</button>
   </div>
 
 }
